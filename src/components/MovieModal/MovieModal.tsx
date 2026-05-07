993be0d0c992566a2e742c1cf0570d8352569257
Ign1s123 <genamisinskij@gmail.com>
2026-05-08 01:05:10 +0300

    #

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import css from "./MovieModal.module.css"
 import type { Movie } from '../../types/movie'
 import { createPortal } from 'react-dom'
